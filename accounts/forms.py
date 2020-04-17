f63b775a86d42f4c8aa0a5a7c905a6d78640caa5
Ubuntu <ubuntu@ip-172-31-93-55.ec2.internal>
2020-04-17 10:30:20 +0000

    sign up form added

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,20 @@
 from django import forms
-
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 class UserLoginForm(forms.Form):
     """Form to login users"""
     
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistrationForm(UserCreationForm):
+    """Form to sign up a new user"""
+    
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password1', 'password2']
