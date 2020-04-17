6a6ad0b1f4c60e82c008ed907d04b3dea2542fbf
Ubuntu <ubuntu@ip-172-31-93-55.ec2.internal>
2020-04-17 07:29:26 +0000

    wired up authentication and login

--- accounts/views.py
+++ accounts/views.py
@@ -15,5 +15,20 @@ def logout(request):
     
 def login(request):
     """Return login page"""
-    login_form = UserLoginForm()
+    if request.method == "POST":
+        login_form = UserLoginForm(request.POST)
+        
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password'])
+                                     
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!")   
+            else:
+                login_form.add_error(None, "Your username or pasword is incorrect")
+    else:
+        login_form = UserLoginForm()
     return render(request, 'login.html', {"login_form": login_form})
+    
+    
