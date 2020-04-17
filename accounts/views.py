81c738acc7dbb3d3f405cc5c7f561b4ded325023
Ubuntu <ubuntu@ip-172-31-93-55.ec2.internal>
2020-04-17 02:36:36 +0000

    logout link activated

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth
 
 # Create your views here.
 def index(request):
     """Return index.html"""
     return render(request, 'index.html')
+
+def logout(request):
+    """Log out the user"""
+    auth.logout(request)
+    return redirect(reverse('index'))
