b1e7dd26216d8b6739da5ee39e1651a31443d482
Ubuntu <ubuntu@ip-172-31-93-55.ec2.internal>
2020-04-21 08:15:50 +0000

    gmail settings and variables added for outgoing password reset email

--- cinebox/settings.py
+++ cinebox/settings.py
@@ -124,4 +124,10 @@ STATIC_URL = '/static/'
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
