e0198f4f615bc1214fc8df443722fdcca5c62284
Ubuntu <ubuntu@ip-172-31-93-55.ec2.internal>
2020-04-22 06:55:55 +0000

    add quantity functionality added

--- movies/models.py
+++ movies/models.py
@@ -5,7 +5,7 @@ class Movie(models.Model):
     name = models.CharField(max_length=50, default='')
     release = models.CharField(max_length=4, default='')
     trailer = models.CharField(max_length=254, default='')
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
     image = models.ImageField(upload_to='images')
     
     def __str__(self):
