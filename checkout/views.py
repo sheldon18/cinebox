d4c135ce83d10dd871b786297a0db951fbdf6f65
Ubuntu <ubuntu@ip-172-31-93-55.ec2.internal>
2020-04-23 02:11:01 +0000

    checkout forms, models and views added

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import OrderForm, MakePaymentForm
+from django.conf import settings
+from django.utils import timezone
+from movies.models import Movie
+from .models import OrderLineItem
+import stripe
 
 # Create your views here.
+
+
+stripe.api_key = settings.STRIPE_SECRET
+
+@login_required()
+def checkout(request):
+    if request.method=="POST":
+        order_form = OrderForm(request.POST)
+        payment_form = MakePaymentForm(request.POST)
+        
+        if order_form.is_valid() and payment_form.is_valid():
+            order = order_form.save(commit=False)
+            order.date = timezone.now()
+            order.save()
+            
+            cart = request.session.get('cart', {})
+            total = 0
+            for id, quantity in cart.items():
+                movie = get_object_or_404(Movie, pk=id)
+                total += quantity * movie.price
+                order_line_item = OrderLineItem(
+                    order = order,
+                    movie = movie,
+                    quantity = quantity
+                    )
+                    
+                order_line_item.save()
+            
+                
+            try:
+                customer = stripe.Charge.create(
+                    amount = int(total * 100),
+                    currency = "USD",
+                    description = request.user.email,
+                    card = payment_form.cleaned_data['stripe_id'],
+                    )
+            except stripe.error.CardError:
+                messages.error(request, "Your card was declined")
+            
+                
+            if customer.paid:
+                messages.error(request, "You payment has been completed!")
+                request.session['cart'] = {}
+                return redirect(reverse('movies'))
+            else:
+                messages.error(request, "Unable to take payment")
+        
+        else:
+            print(payment_form.errors)
+            messages.error(request, "We were unable to take a payment with that card.")
+            
+    else:
+        payment_form = MakePaymentForm()
+        order_form = OrderForm()
+        
+    return render(request, "checkout.html", {'order_form': order_form, 'payment_form': payment_form, 'publishable': settings.STRIPE_PUBLISHABLE})    
