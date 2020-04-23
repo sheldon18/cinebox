313645c37914d8cb5687793dae52fe08e835aea4
Ubuntu <ubuntu@ip-172-31-93-55.ec2.internal>
2020-04-23 10:25:59 +0000

    S3 added to project

--- cinebox/settings.py
+++ cinebox/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'cart',
     'search',
     'checkout',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -131,6 +132,22 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000',
+}
+
+AWS_STORAGE_BUCKET_NAME = 'cinebox'
+AWS_S3_REGION_NAME = 'ap-southeast-2'
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
